9df0425eb636d4a69b6a17e56986582523d32a22
nonghoangls <nonghoangxyz@gmail.com>
2023-12-12 15:47:17 +0700

    initcode

---
+++ src/test/java/com/hoangfcb/SpringbootBackend/SpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoangfcb.SpringbootBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
